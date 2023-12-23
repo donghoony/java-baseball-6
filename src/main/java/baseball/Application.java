93caa25888bc76f651939aeafb0fecb7848da0bf
donghoony <aru0504@naver.com>
2023-12-24 02:54:52 +0900

    feat(Application): 도메인 생성, 클라이언트 실행 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.generator.NumberGenerator;
+import baseball.generator.RandomNumberGenerator;
+import baseball.io.BaseballConsoleInput;
+import baseball.io.BaseballConsoleOutput;
+import baseball.io.BaseballInput;
+import baseball.io.BaseballOutput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballInput input = new BaseballConsoleInput();
+        BaseballOutput output = new BaseballConsoleOutput();
+        NumberGenerator generator = new RandomNumberGenerator();
+        Computer computer = new Computer(generator);
+
+        BaseballClient baseballClient = new BaseballClient(input, output, computer);
+        do {
+            baseballClient.play();
+        } while (baseballClient.askForRetry());
     }
 }
